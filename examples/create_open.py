a6e7e426b2dee7eeb078d95f0cef64b2c35ab486
Youjia Li <lyoujia17@gmail.com>
2023-04-30 17:44:21 -0500

    Added fill_mode example program

--- examples/create_open.py
+++ examples/create_open.py
@@ -38,14 +38,12 @@ def parse_help():
                 "Usage: {} [-h] | [-q] [file_name]\n"
                 "       [-h] Print help\n"
                 "       [-q] Quiet mode (reports when fail)\n"
-                "       [filename] output netCDF file name\n"
+                "       [filename] (Optional) output netCDF file name\n"
             ).format(sys.argv[0])
             print(help_text)
         return True
     else:
         False
-
-
 
 def pnetcdf_check_mem_usage(comm):
     malloc_size, sum_size = 0, 0
@@ -87,8 +85,7 @@ def main():
     f = pncpy.File(filename=filename, mode = 'r', Comm=comm, Info=None)
     # close the file
     f.close()
-    # pnetcdf_check_mem_usage(comm)
-    # TODO: re-install PnetCDF C to enable internal memory tracking
+    pnetcdf_check_mem_usage(comm)
 
 if __name__ == "__main__":
     main()
