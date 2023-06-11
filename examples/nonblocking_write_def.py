d71afe82e82861b4993894aae7e449f78e548653
Youjia Li <lyoujia17@gmail.com>
2023-06-11 17:55:47 -0500

    Fixed example program and added new ones

--- examples/nonblocking_write_def.py
+++ examples/nonblocking_write_def.py
@@ -5,20 +5,21 @@
 # 
 ####################################################################
 """
-    This example is similar to collective_write.py but using nonblocking APIs.
+    This example is the same as nonblocking_write.py expect all nonblocking
+    write requests (calls to iput and bput) are posted in define mode.
      It creates a netcdf file in CD-5 format and writes a number of
      3D integer non-record variables. The measured write bandwidth is reported
      at the end. Usage: (for example)
 
      To run:
-         mpiexec -n num_processes nonblocking_write.py [filename] [len]
+         mpiexec -n num_processes nonblocking_write_def.py [filename] [len]
          
      where len decides the size of each local array, which is len x len x len.
      So, each non-record variable is of size len*len*len * nprocs * sizeof(int)
      All variables are partitioned among all processes in a 3D
      block-block-block fashion. Below is an example standard output from
      command:
-         mpiexec -n 32 python3 nonblocking_write_.py tmp/test1.nc 100
+         mpiexec -n 32 python3 nonblocking_write_def.py tmp/test1.nc -l 100
  
      MPI hint: cb_nodes        = 2
      MPI hint: cb_buffer_size  = 16777216
@@ -88,91 +89,6 @@ def pnetcdf_check_mem_usage(comm):
         sum_size = comm.reduce(malloc_size, MPI.SUM, root=0)
         if rank == 0 and sum_size > 0:
             print("Heap memory allocated by PnetCDF internally has {} bytes yet to be freed".format(sum_size))
-
-def pnetcdf_io(comm, filename, file_format, length):
-    rank = comm.Get_rank()
-    nprocs = comm.Get_size()
-
-    starts = np.zeros(NDIMS, dtype=np.int32)
-    counts = np.zeros(NDIMS, dtype=np.int32)
-    gsizes = np.zeros(NDIMS, dtype=np.int32)
-    buf = []
-
-    psizes = MPI.Compute_dims(nprocs, NDIMS)
-    starts[0] = rank % psizes[0]
-    starts[1] = (rank // psizes[1]) % psizes[1]
-    starts[2] = (rank // (psizes[0] * psizes[1])) % psizes[2]
-
-    bufsize = 1
-    for i in range(NDIMS):
-        gsizes[i] = length * psizes[i]
-        starts[i] *= length
-        counts[i] = length
-        bufsize *= length
-
-    # Allocate buffer and initialize with non-zero numbers
-    for i in range(NUM_VARS):
-        buf.append(np.empty(bufsize, dtype=np.int32))
-        for j in range(bufsize):
-            buf[i][j] = rank * i + 123 + j
-
-    comm.Barrier()
-    write_timing = MPI.Wtime()
-
-    # Create the file
-    try:
-        f = pncpy.File(filename=filename, mode = 'w', format = file_format, comm=comm, info=None)
-    except OSError as e:
-        print("Error at {}:{} ncmpi_create() file {} ({})".format(__file__,inspect.currentframe().f_back.f_lineno, filename, e))
-        MPI.Abort(comm, -1)
-        exit(1)
-
-    # Define dimensions
-    dims = []
-    for i in range(NDIMS):
-        dim = f.def_dim(chr(ord('x')+i), gsizes[i])
-        dims.append(dim)
-
-    # Define variables
-    vars = []
-    for i in range(NUM_VARS):
-        var = f.def_var("var{}".format(i), pncpy.NC_INT, dims)
-        vars.append(var)
-    # Exit the define mode
-    f.enddef()
-
-    # Get all the hints used
-    info_used = f.inq_info()
-
-    # Write one variable at a time
-    for i in range(NUM_VARS):
-        vars[i].put_var_all(buf[i], start = starts, count = counts)
-
-    # Close the file
-    f.close()
-
-    write_timing = MPI.Wtime() - write_timing
-
-    write_size = bufsize * NUM_VARS * np.dtype(np.int32).itemsize
-
-    for i in range(NUM_VARS):
-        buf[i] = None
-
-    sum_write_size = comm.reduce(write_size, MPI.SUM, root=0)
-    max_write_timing = comm.reduce(write_timing, MPI.MAX, root=0)
-
-    if rank == 0 and verbose:
-        subarray_size = (bufsize * np.dtype(np.int32).itemsize) / 1048576.0
-        print_info(info_used)
-        print("Local array size {} x {} x {} integers, size = {:.2f} MB".format(length, length, length, subarray_size))
-        sum_write_size /= 1048576.0
-        print("Global array size {} x {} x {} integers, write size = {:.2f} GB".format(gsizes[0], gsizes[1], gsizes[2], sum_write_size/1024.0))
-
-        write_bw = sum_write_size / max_write_timing
-        print(" procs    Global array size  exec(sec)  write(MB/s)")
-        print("-------  ------------------  ---------  -----------")
-        print(" {:4d}    {:4d} x {:4d} x {:4d} {:8.2f}  {:10.2f}\n".format(nprocs, gsizes[0], gsizes[1], gsizes[2], max_write_timing, write_bw))
-
 
 def main():
     comm = MPI.COMM_WORLD
@@ -249,24 +165,15 @@ def main():
     for i in range(NUM_VARS):
         var = f.def_var("var{}".format(i), pncpy.NC_INT, dims)
         vars.append(var)
-    # Exit the define mode
-    f.enddef()
 
-    # Get all the hints used
-    info_used = f.inq_info()
 
     # Write one variable at a time
-    reqs = []
     for i in range(NUM_VARS):
-        req_id = vars[i].iput_var(buf[i], start = starts, count = counts)
-        reqs.append(req_id)
-    req_errs = [None] * NUM_VARS
-    f.wait_all(NUM_VARS, reqs, req_errs)
+        vars[i].iput_var(buf[i], start = starts, count = counts)
+    # Enter data mode
+    f.enddef()
+    f.wait_all(num = pncpy.NC_REQ_ALL)
     # Close the file
-
-    for i in range(NUM_VARS):
-        if strerrno(req_errs[i]) != "NC_NOERR":
-            print(f"Error on request {i}:",  strerror(req_errs[i]))
 
     bbufsize = bufsize * NUM_VARS * np.dtype(np.int32).itemsize
     f.attach_buff(bbufsize)
@@ -286,6 +193,8 @@ def main():
     # detach the temporary buffer
     f.detach_buff()
     put_size = f.inq_put_size()
+    # Get all the hints used
+    info_used = f.inq_info()
     put_size = comm.allreduce(put_size, op=MPI.SUM)
     f.close()
 
