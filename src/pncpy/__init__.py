0e843bb34b8f653c734a991c7a2a0590a63996a9
Youjia Li <lyoujia17@gmail.com>
2023-09-03 23:03:34 -0500

    Upgraded version number to 0.0.3 for testing on TestPyPI

--- src/pncpy/__init__.py
+++ src/pncpy/__init__.py
@@ -1,6 +1,6 @@
 # init for pncpy. package
 # Docstring comes from extension module _PnetCDF.
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 from ._File import *
 from ._Dimension import *
 from ._Variable import *
