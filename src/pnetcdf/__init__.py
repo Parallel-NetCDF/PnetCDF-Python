b44227ec505f4f5e86580b7ce8298ae9008ded9e
Wei-keng Liao <wkliao@users.noreply.github.com>
2024-09-10 20:30:18 -0500

    set version number to 1.0.0, preparing for a new release

--- src/pnetcdf/__init__.py
+++ src/pnetcdf/__init__.py
@@ -7,7 +7,7 @@
 
 # init for pnetcdf. package
 # Docstring comes from extension module _PnetCDF.
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 from ._File import *
 from ._Dimension import *
 from ._Variable import *
