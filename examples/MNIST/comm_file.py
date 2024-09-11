a60aa15bb2c73d2597c26da956e92b2b13dd0074
Wei-keng Liao <wkliao@users.noreply.github.com>
2024-09-11 02:34:17 -0500

    add copyright banner

--- examples/MNIST/comm_file.py
+++ examples/MNIST/comm_file.py
@@ -1,3 +1,8 @@
+#
+# Copyright (C) 2024, Northwestern University and Argonne National Laboratory
+# See COPYRIGHT notice in top-level directory.
+#
+
 import os
 import torch
 import torch.distributed as dist
